5d6ac59aff0c435dd662785a3320a94eae4ec210
kooler62 <kooler62@gmail.com>
2017-06-24 18:28:00 +0300

    6 less NOT WORK user did not auth

--- src/AppBundle/Security/LoginFormAuthenticator.php
+++ src/AppBundle/Security/LoginFormAuthenticator.php
@@ -4,22 +4,32 @@ namespace AppBundle\Security;
 
 
 use AppBundle\Form\LoginForm;
+use Doctrine\ORM\EntityManager;
 use Symfony\Component\Form\FormFactoryInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Core\User\UserProviderInterface;
 use Symfony\Component\Security\Guard\Authenticator\AbstractFormLoginAuthenticator;
+use Symfony\Component\Routing\RouterInterface;
+
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Http\Util\TargetPathTrait;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class LoginFormAuthenticator extends AbstractFormLoginAuthenticator
 {
-
-
+    use TargetPathTrait;
+    private $em;
     private $formFactory;
+    private $router;
 
-    public function __construct(FormFactoryInterface $formFactory)
+    public function __construct(FormFactoryInterface $formFactory,EntityManager $em, RouterInterface $router)
     {
         $this->formFactory = $formFactory;
+        $this->em = $em;
+
+        $this->router = $router;
     }
 
 
@@ -32,25 +42,52 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator
 
         $form=$this->formFactory->create(LoginForm::class);
         $form->handleRequest($request);
-        $data=$form->setData();
+        $data=$form->getData();
         return $data;
 
     }
 
     public function getUser($credentials, UserProviderInterface $userProvider)
     {
-        // TODO: Implement getUser() method.
+        $username=$credentials['_username'];
+        return $this->em->getRepository('AppBundle:User')
+            ->findOneBy(['email'=>$username]);
     }
 
     public function checkCredentials($credentials, UserInterface $user)
     {
-        // TODO: Implement checkCredentials() method.
+        $password=$credentials['_password'];
+        if ($password=='111111'){
+
+            return true;
+
+        }
+        return false;
     }
 
+    //bad auht
     protected function getLoginUrl()
     {
-        // TODO: Implement getLoginUrl() method.
+        dump('bad');
+        return $this->router->generate('security_login');
+
     }
+
+
+
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token, $providerKey)
+    {
+        // if the user hits a secure page and start() was called, this was
+        // the URL they were on, and probably where you want to redirect to
+        $targetPath = $this->getTargetPath($request->getSession(), $providerKey);
+
+        if (!$targetPath) {
+            $targetPath = $this->router->generate('homepage');
+        }
+
+        return new RedirectResponse($targetPath);
+    }
+
 
 
 }
