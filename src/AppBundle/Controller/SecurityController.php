64c38a982b96aff3a19a98ef0fead7777be0bb5e
kooler62 <kooler62@gmail.com>
2017-06-24 13:41:16 +0300

    3 less complete: render login-form

---
+++ src/AppBundle/Controller/SecurityController.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kooler62
+ * Date: 24.06.17
+ * Time: 12:52
+ */
+
+namespace AppBundle\Controller;
+
+
+use AppBundle\Form\LoginForm;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class SecurityController extends Controller
+{
+    /**
+     * @Route("/login", name="security_login")
+     */
+    public function loginAction()
+    {
+        $authenticationUtils = $this->get('security.authentication_utils');
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        $form=$this->createForm(LoginForm::class,[
+            '_username'=>$lastUsername
+        ]);
+        return $this->render(
+            'security/login.html.twig',
+            array(
+                // last username entered by the user
+                'form'=>$form->createView(),
+                'error'         => $error,
+            )
+        );
+    }
+}
